fbd9e6a452fee3aa9c7b60a33a815a0945b5cbe1
sneharajpurkar <devshdighe4d@gmail.com>
2024-01-06 15:49:55 +0530

    updates

---
+++ dsa js/index.js
@@ -0,0 +1,109 @@
+// Q 1 . **************
+
+
+// let arr = [3,2,2,3]
+// let val = 3;
+// let res = 0;
+
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i]==val){
+//         arr[i]=arr[i+1];
+//         res = res + 1;
+//     }
+//     else{
+        
+//     }
+// }
+
+// console.log(arr);
+// console.log(res);    //Ans
+
+
+
+
+
+// // Q 3 *****************8
+
+// let arr = [1,2,3,4,5];
+// let ans = 0;
+
+// function sum (arr){
+//     for(val of arr){
+//         ans = ans + val
+//     }
+//     return ans;
+// }
+
+// console.log(sum(arr));
+
+
+
+// // Q 5 
+// let arr = [2, 4, 6, 7];
+// let newarr = [ ];
+
+// function reverseArr(arr, newarr){
+//     for(let i = 0; i<arr.length; i++){
+//         newarr.unshift(arr[i])
+//     }
+// }
+
+// console.log(arr);
+// reverseArr(arr ,newarr);
+// console.log(newarr);                    //Ans
+
+
+
+// // // Q 6
+
+// let arr = [1, 3 ,5 ,3 ,2 ,1]
+
+
+// function remvDup(arr){
+//     let removedDuplicate = []
+//     for(let i = 0; i>arr.length; i++){
+//         for(let j = i + 1; j>arr.length; j++){
+//             if(arr[i] == arr[j]){
+//                 removedDuplicate.push(arr[i])
+
+//             }
+//         }
+//     }
+//     return removedDuplicate
+// }
+
+// console.log(remvDup(arr));
+
+
+
+
+// // Q 10
+
+// let arr = [2, 4 , 1, 8, 3]
+
+// function sortedArr(arr){
+//     let sorted = arr.sort()
+//     return sorted
+// }
+
+
+// console.log(sortedArr(arr));  // Ans
+
+// // Q 12
+
+// let haystack = 'sadbutsad';
+// let needle = 'sad'
+
+// let result = haystack.indexOf(needle)
+// console.log(result);                        // Ans
+
+
+// Q 15
+let x = 'hello'
+
+let a = x.split('')
+console.log(a);
+
+for(let i = 0; i > a.length; i++){
+    for(let j = j >)
+}
